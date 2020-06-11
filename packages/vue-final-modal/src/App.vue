f90d9341c880d9d4889816bb543e0b765dee0912
Hunter <hunterliu1003@gmail.com>
2020-06-11 14:51:21 +0800

    update VFinalModal

--- packages/vue-final-modal/src/App.vue
+++ packages/vue-final-modal/src/App.vue
@@ -1,16 +1,36 @@
 <template>
   <div id="app">
-    <VFinalModal></VFinalModal>
+    <VFinalModal v-if="showModal" v-model="showModal">
+      <BaseButton @click="showModal = false">close 1</BaseButton>
+      <VFinalModal v-model="showModal2" :lock-body-scroll="false" persistent>
+        <BaseButton @click="showModal2 = false">close 2</BaseButton>
+        <VFinalModal v-if="showModal3" v-model="showModal3" hide-overlay>
+          <BaseButton @click="showModal3 = false">close 3</BaseButton>
+        </VFinalModal>
+        <BaseButton @click="showModal3 = true">Show third modal</BaseButton>
+      </VFinalModal>
+      <BaseButton @click="showModal2 = true">Show second modal</BaseButton>
+    </VFinalModal>
+    <BaseButton @click="showModal = true">Show default modal</BaseButton>
+    <VFinalModal v-if="showModal4" v-model="showModal4" hide-overlay>
+      <BaseButton @click="showModal4 = false">close 4</BaseButton>
+    </VFinalModal>
+    <BaseButton @click="showModal4 = true">Show forth modal</BaseButton>
+    <div v-for="i in 100" :key="i">{{ i }}</div>
   </div>
 </template>
 
 <script>
-import VFinalModal from '@/components/VFinalModal.vue'
-
+import VFinalModal from '@/components/VFinalModal'
 export default {
-  name: 'app',
   components: {
     VFinalModal
-  }
+  },
+  data: () => ({
+    showModal: false,
+    showModal2: false,
+    showModal3: false,
+    showModal4: false
+  })
 }
 </script>
--- packages/vue-final-modal/src/components/VFinalModal.vue
+++ packages/vue-final-modal/src/components/VFinalModal.vue
@@ -1,3 +1,142 @@
 <template>
-  <div>v-final-modal</div>
+  <div
+    v-show="value"
+    :class="classes"
+    @click="!persistent && $emit('input', false)"
+  >
+    <div :class="contentClasses" @click.stop>
+      <slot />
+    </div>
+  </div>
 </template>
+
+<script>
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
+
+let modalStack = []
+let overlay = (() => {
+  let overlay = document.createElement('div')
+  overlay.className = 'vfm-overlay'
+  return overlay
+})()
+
+export default {
+  name: 'TModal',
+  props: {
+    value: { type: Boolean, default: false },
+    lockBodyScroll: { type: Boolean, default: true },
+    appendToBody: { type: Boolean, default: true },
+    hideOverlay: { type: Boolean, default: false },
+    persistent: { type: Boolean, default: false },
+    contentClass: { type: String, default: '' }
+  },
+  computed: {
+    classes() {
+      return {
+        'vfm-container': true
+      }
+    },
+    contentClasses() {
+      return `vfm-content ${this.contentClass}`.trim()
+    }
+  },
+  watch: {
+    value: 'init',
+    lockBodyScroll: 'handleScrollLock'
+  },
+  created() {
+    setTimeout(() => {
+      this.mounted(this.value)
+    }, 0)
+  },
+  beforeDestroy() {
+    this.lockBodyScroll && enableBodyScroll()
+  },
+  destroyed() {
+    // if appendToBody is true, remove DOM node after destroy
+    if (this.appendToBody && this.$el && this.$el.parentNode) {
+      this.$el.parentNode.removeChild(this.$el)
+    }
+    this.close()
+  },
+  methods: {
+    init(value) {
+      if (value) {
+        this.appendToBody && document.body.appendChild(this.$el)
+        modalStack.push(this)
+        this.handleScrollLock()
+        this.hideOverlay ? this.removeOverlay() : this.appendOverlay()
+      } else {
+        this.lockBodyScroll && enableBodyScroll()
+        this.close()
+      }
+    },
+    mounted(value) {
+      if (value) {
+        this.appendToBody && document.body.appendChild(this.$el)
+        modalStack.push(this)
+        this.handleScrollLock()
+        this.hideOverlay ? this.removeOverlay() : this.appendOverlay()
+      } else {
+        this.lockBodyScroll && enableBodyScroll()
+      }
+    },
+    close() {
+      modalStack.pop()
+      if (modalStack.length > 0) {
+        const $_vm = modalStack[modalStack.length - 1]
+        $_vm.handleScrollLock()
+        $_vm.hideOverlay ? $_vm.removeOverlay() : $_vm.appendOverlay()
+      } else {
+        !this.hideOverlay && this.removeOverlay()
+      }
+      this.$emit('input', false)
+    },
+    appendOverlay() {
+      this.$el && this.$el.before(overlay, this.$el)
+    },
+    removeOverlay() {
+      overlay && overlay.parentNode && overlay.parentNode.removeChild(overlay)
+    },
+    handleScrollLock() {
+      this.lockBodyScroll ? disableBodyScroll() : enableBodyScroll()
+    }
+  }
+}
+</script>
+
+<style>
+.vfm-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #212121;
+  opacity: 0.46;
+}
+</style>
+
+<style lang="scss" scoped>
+.vfm-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+.vfm-content {
+  max-height: 90%;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 4px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  overflow: auto;
+}
+</style>
