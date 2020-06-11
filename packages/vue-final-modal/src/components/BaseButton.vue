7a125c1bb7763df381d0bcccd7c47549bb14c3bb
Hunter <hunterliu1003@gmail.com>
2020-06-11 14:51:05 +0800

    add BaseButton

---
+++ packages/vue-final-modal/src/components/BaseButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="base-button" v-on="$listeners">
+    <slot></slot>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.base-button {
+  @apply mx-1 px-2 py-1 border border-gray-700 rounded;
+  &:hover {
+    @apply bg-gray-400;
+  }
+  &:focus {
+    @apply outline-none;
+  }
+}
+</style>
