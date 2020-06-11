0463ee2269bf994ffd48e4a0f035b5c17bf153ff
Hunter <hunterliu1003@gmail.com>
2020-06-11 17:14:20 +0800

    v0.1.1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <template>
   <div id="app">
-    <VFinalModal v-if="showModal" v-model="showModal">
+    <VueFinalModal v-if="showModal" v-model="showModal">
       <BaseButton @click="showModal = false">close 1</BaseButton>
-      <VFinalModal v-model="showModal2" :lock-body-scroll="false" persistent>
+      <VueFinalModal v-model="showModal2" :lock-body-scroll="false" persistent>
         <BaseButton @click="showModal2 = false">close 2</BaseButton>
-        <VFinalModal v-if="showModal3" v-model="showModal3" hide-overlay>
+        <VueFinalModal v-if="showModal3" v-model="showModal3" hide-overlay>
           <BaseButton @click="showModal3 = false">close 3</BaseButton>
-        </VFinalModal>
+        </VueFinalModal>
         <BaseButton @click="showModal3 = true">Show third modal</BaseButton>
-      </VFinalModal>
+      </VueFinalModal>
       <BaseButton @click="showModal2 = true">Show second modal</BaseButton>
-    </VFinalModal>
+    </VueFinalModal>
     <BaseButton @click="showModal = true">Show default modal</BaseButton>
-    <VFinalModal v-if="showModal4" v-model="showModal4" hide-overlay>
+    <VueFinalModal v-if="showModal4" v-model="showModal4" hide-overlay>
       <BaseButton @click="showModal4 = false">close 4</BaseButton>
-    </VFinalModal>
+    </VueFinalModal>
     <BaseButton @click="showModal4 = true">Show forth modal</BaseButton>
     <div v-for="i in 100" :key="i">{{ i }}</div>
   </div>
 </template>
 
 <script>
-import VFinalModal from '@/components/VFinalModal'
+import VueFinalModal from '@/components/VueFinalModal'
 export default {
   components: {
-    VFinalModal
+    VueFinalModal
   },
   data: () => ({
     showModal: false,
