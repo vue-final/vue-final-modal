6277f2b106d10b040631d1763a9ac961e6cde723
Hunter <hunterliu1003@gmail.com>
2020-06-11 17:51:06 +0800

    fixed publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const VueAutomaticImportPlugin = require('vue-automatic-import-loader/lib/plugin')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-final-modal/' : '/',
   configureWebpack: {
     plugins: [
       new VueAutomaticImportPlugin({
