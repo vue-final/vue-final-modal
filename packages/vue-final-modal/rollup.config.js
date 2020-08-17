707d7b399b3338b654165ac7979b925335040cba
Hunter <hunterliu1003@gmail.com>
2020-08-17 22:07:49 +0800

    v0.10.5

--- packages/vue-final-modal/rollup.config.js
+++ packages/vue-final-modal/rollup.config.js
@@ -5,7 +5,7 @@ import babel from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 import cleanup from 'rollup-plugin-cleanup'
 
-const defaultPath = 'lib/'
+const defaultPath = '../../lib/'
 const defaultName = 'VueFinalModal'
 
 const defaultPlugins = [
