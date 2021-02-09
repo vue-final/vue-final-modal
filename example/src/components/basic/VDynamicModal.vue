0d6c2825963edbf44677f5e7ccf36cb19a513885
Hunter <hunterliu1003@gmail.com>
2021-02-09 18:54:27 +0800

    feat: upgrade dependencies

--- example/src/components/basic/VDynamicModal.vue
+++ example/src/components/basic/VDynamicModal.vue
@@ -29,12 +29,12 @@ export default {
 </script>
 
 <style scoped>
-::v-deep .modal-container {
+::v-deep(.modal-container) {
   display: flex;
   justify-content: center;
   align-items: center;
 }
-::v-deep .modal-content {
+::v-deep(.modal-content) {
   position: relative;
   display: flex;
   flex-direction: column;
@@ -57,7 +57,7 @@ export default {
 </style>
 
 <style scoped>
-.dark-mode div::v-deep .modal-content {
+.dark-mode div::v-deep(.modal-content) {
   border-color: #2d3748;
   background-color: #1a202c;
 }
