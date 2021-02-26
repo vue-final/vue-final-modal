ae951bb158c1f8bbf4cf4799c8f0f4a7c70e12af
Hunter <hunterliu1003@gmail.com>
2021-02-26 16:30:33 +0800

    feat: insatall coveralls and travis

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/no-babel'
+  preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
+  collectCoverage: true,
+  collectCoverageFrom: ['lib/**', '!lib/utils/**']
 }
