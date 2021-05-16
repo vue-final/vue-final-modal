bd98009b7b966ea6a5eb1f24a580f49fa50ae226
Hunter <hunterliu1003@gmail.com>
2021-05-16 13:10:36 +0800

    docs: fix vue-final-modal@2.2.0

--- example/src/components/hoc/VTailwindModal.vue
+++ example/src/components/hoc/VTailwindModal.vue
@@ -6,19 +6,15 @@
     content-class="relative flex flex-col max-h-full mx-4 p-4 border dark:border-gray-800 rounded bg-white dark:bg-gray-900"
     v-on="$listeners"
   >
-    <span class="mr-8 text-2xl font-bold ">
+    <span class="mr-8 text-2xl font-bold">
       <slot name="title"></slot>
     </span>
     <div class="flex-grow overflow-y-auto">
       <slot v-bind:params="params"></slot>
     </div>
     <div class="flex-shrink-0 flex justify-center items-center pt-4">
-      <button class="vfm-btn" @click="$emit('confirm', close)">
-        confirm
-      </button>
-      <button class="vfm-btn" @click="$emit('cancel', close)">
-        cancel
-      </button>
+      <button class="vfm-btn" @click="$emit('confirm', close)">confirm</button>
+      <button class="vfm-btn" @click="$emit('cancel', close)">cancel</button>
     </div>
     <button class="absolute top-0 right-0 mt-2 mr-2" @click="close">
       <mdi-close></mdi-close>
