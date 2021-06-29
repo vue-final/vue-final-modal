4f8e390b2fa248918ec07563bc8c30f40ffbb60d
Hunter <hunterliu1003@gmail.com>
2021-06-29 11:15:41 +0800

    test: add test case about globally register vue-final-modal plugin

--- tests/unit/Plugin.spec.js
+++ tests/unit/Plugin.spec.js
@@ -1,9 +1,29 @@
 import { createLocalVue, enableAutoDestroy } from '@vue/test-utils'
-import { vfmPlugin } from '../../lib'
+import VueFinalModal, { vfmPlugin } from '../../lib'
 
 enableAutoDestroy(afterEach)
 
 describe('Plugin', () => {
+  it('globally register vue-final-modal', () => {
+    const localVue = createLocalVue()
+    localVue.use(VueFinalModal())
+    expect(localVue.options.components).toHaveProperty('VueFinalModal')
+    expect(localVue.options.components).toHaveProperty('ModalsContainer')
+    expect(localVue.prototype).toHaveProperty('$vfm')
+  })
+  it('globally register vue-final-modal by customized options', () => {
+    const localVue = createLocalVue()
+    localVue.use(
+      VueFinalModal({
+        key: '_$vfm',
+        componentName: 'MyModal',
+        dynamicContainerName: 'MyModalsContainer'
+      })
+    )
+    expect(localVue.options.components).toHaveProperty('MyModal')
+    expect(localVue.options.components).toHaveProperty('MyModalsContainer')
+    expect(localVue.prototype).toHaveProperty('_$vfm')
+  })
   it('window is undefined in node server', () => {
     Object.defineProperty(global, 'window', { value: undefined })
     global.console.error = jest.fn()
