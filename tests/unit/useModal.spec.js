89cfaec1598cda9ce07606b4b5f9374e39002826
Hunter <hunterliu1003@gmail.com>
2021-08-10 18:21:52 +0800

    test: add useModal unit test

---
+++ tests/unit/useModal.spec.js
@@ -0,0 +1,28 @@
+import { useModal } from '../../lib'
+import { initDynamicModal } from './utils'
+
+describe('useModal', () => {
+  it('open modal with useModal', async () => {
+    const { wrapper } = await initDynamicModal()
+    const string = 'testUseModal'
+    const modal = useModal({
+      slots: {
+        default: string
+      }
+    })
+
+    await modal.show()
+    expect(wrapper.find('.vfm').html()).toContain(string)
+    
+    const showResponse = await modal.show()
+    expect(showResponse).toEqual('[Vue Final Modal] modal is already opened')
+
+    await modal.hide()
+    expect(wrapper.find('.vfm').exists()).toBe(false)
+    
+    const hideResponse = await modal.hide()
+    expect(hideResponse).toEqual('[Vue Final Modal] modal is already closed')
+
+    wrapper.unmount()
+  })
+})
