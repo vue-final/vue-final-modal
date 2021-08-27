3b0fad5b3d1ded3982b97a83da053fd6ccb73119
Hunter <hunterliu1003@gmail.com>
2021-08-27 12:55:46 +0800

    feat: 1. pnpm 2. travis pnpm 3. update dependencies 4. build lib

--- example/src/main.js
+++ example/src/main.js
@@ -2,15 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'virtual:windi.css'
 
+import { VueFinalModal, ModalsContainer } from 'vue-final-modal'
+
 import VDynamicModal from './components/basic/VDynamicModal.vue'
 import CustomModal from './components/hoc/CustomModal.vue'
 import VTitle from './components/VTitle.vue'
 import VDescription from './components/VDescription.vue'
-import VueFinalModal from 'vue-final-modal'
 
 const app = createApp(App)
 
-app.use(VueFinalModal())
+app.component('VueFinalModal', VueFinalModal)
+app.component('ModalsContainer', ModalsContainer)
+
 app.component('VDynamicModal', VDynamicModal)
 app.component('CustomModal', CustomModal)
 app.component('VTitle', VTitle)
