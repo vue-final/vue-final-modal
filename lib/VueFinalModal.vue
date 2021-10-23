1758ebe4200cc1f1da746fe66d9b5a0e92b942f1
Alex Liu <dsa1314@gmail.com>
2021-10-23 15:24:16 +0800

    refactor: refactor focusTrap

--- lib/VueFinalModal.vue
+++ lib/VueFinalModal.vue
@@ -409,7 +409,7 @@ export default {
         if ($_vm.props.focusTrap) {
           nextTick(() => {
             $_vm.$focusTrap.enable($_vm.vfmContainer.value)
-            $_vm.$focusTrap.firstElement().focus()
+            $_vm.$focusTrap.firstElement.focus()
           })
         }
 
@@ -494,7 +494,7 @@ export default {
     function beforeModalLeave() {
       modalTransitionState.value = TransitionState.Leaving
 
-      if ($focusTrap.enabled()) {
+      if ($focusTrap.isEnabled) {
         $focusTrap.disable()
       }
     }
