a2b24c8012fe66b6c9115bc371a2492f0a9b7e07
Hunter <hunterliu1003@gmail.com>
2022-08-26 16:48:49 +0800

    Revert "refactor: remove code-block, move deprecated lib into src/deprecated/lib"
    This reverts commit f45d93bf04128e88d3755cd35e2c99e711ecaf34.

---
+++ deprecated/packages/code-block/Prism.vue
@@ -0,0 +1,20 @@
+<script>
+import { h } from 'vue'
+import prism from 'prismjs'
+
+export default {
+  setup(props, { slots }) {
+    return () => {
+      const code = prism.highlight(slots.default()[0].children, prism.languages.markup, 'markup')
+
+      return h(
+        'div',
+        {
+          class: 'language-vue'
+        },
+        h('pre', h('code', { innerHTML: code }))
+      )
+    }
+  }
+}
+</script>
