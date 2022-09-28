04b396fd91f9bbba12c435632197832deb046343
Hunter <hunterliu1003@gmail.com>
2022-09-29 00:03:16 +0800

    chore: add nuxt 3 example

---
+++ packages/nuxt3-example/app.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { ModalsContainer, VBottomSheet, VueFinalModal, useModal } from 'vue-final-modal'
+
+const modal = useModal<InstanceType<typeof VBottomSheet>['$props']>({
+  component: markRaw(VBottomSheet),
+  defaultSlot: '123',
+})
+
+onMounted(() => {
+  modal.open()
+})
+
+const show = ref(false)
+</script>
+
+<template>
+  <div>
+    <button @click="show = !show">
+      toggle
+    </button>
+    <VueFinalModal v-model="show">
+      123
+    </VueFinalModal>
+    <ModalsContainer />
+  </div>
+</template>
