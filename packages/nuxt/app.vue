b4ff9c6f0deddde1604f9355ee275b49705e4c8b
Hunter <hunterliu1003@gmail.com>
2022-09-29 02:26:02 +0800

    chore: nuxt windicss and some examples

--- packages/nuxt/app.vue
+++ packages/nuxt/app.vue
@@ -1,8 +1,17 @@
 <script lang="ts" setup>
-import { ModalsContainer, VBottomSheet, VueFinalModal, useModal } from 'vue-final-modal'
+import {
+  ModalsContainer, VBottomSheet, VFullScreen, VueFinalModal, useModal,
+} from 'vue-final-modal'
 
 const modal = useModal<InstanceType<typeof VBottomSheet>['$props']>({
   component: markRaw(VBottomSheet),
+  attrs: {
+    bottomSheetStyle: {
+      height: '500px',
+      backgroundColor: '#fff',
+    },
+    closeDirection: 'DOWN',
+  },
   slots: { default: '123' },
 })
 
@@ -11,16 +20,37 @@ onMounted(() => {
 })
 
 const show = ref(false)
+const show2 = ref(false)
 </script>
 
 <template>
-  <div>
-    <button @click="show = !show">
-      toggle
+  <div class="p-4 space-x-4">
+    <button class="px-2 border border-dark-50 rounded" @click="show = !show">
+      open VueFinalModal
     </button>
-    <VueFinalModal v-model="show">
-      123
+    <VueFinalModal v-model="show" class="flex justify-center items-center" content-class="p-4 bg-white rounded space-y-2 ">
+      <h1 class="text-lg">
+        A SSR Full Screen Modal
+      </h1>
+      <p>with some paragraphs</p>
+      <button class="block ml-auto px-2 border border-dark-50 rounded" @click="show = false">
+        Close
+      </button>
     </VueFinalModal>
+
+    <button class="px-2 border border-dark-50 rounded" @click="show = !show">
+      open FullScreen Modal
+    </button>
+
+    <VFullScreen v-model="show2" full-screen-class="p-4 bg-white space-y-2">
+      <h1 class="text-lg">
+        A SSR Full Screen Modal
+      </h1>
+      <p>with some paragraphs</p>
+      <button class="py-1 px-2 border border-dark-50 rounded" @click="show2 = false">
+        Close
+      </button>
+    </VFullScreen>
     <ModalsContainer />
   </div>
 </template>
--- packages/nuxt/nuxt.config.ts
+++ packages/nuxt/nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  modules: [
+    'nuxt-windicss',
+  ],
 })
