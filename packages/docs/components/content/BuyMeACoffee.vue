a3326d0f3f1eee184c9581b17d9a36d31ff3531e
Hunter <hunterliu1003@gmail.com>
2022-10-31 13:01:28 +0800

    docs: add buy me a coffee

---
+++ packages/docs/components/content/BuyMeACoffee.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex justify-end">
+    <a href="https://www.buymeacoffee.com/PL2qJIx" target="_blank" rel="noopener noreferrer">
+      <img width="200" src="https://cdn.buymeacoffee.com/buttons/v2/default-green.png" alt="Buy Me A Coffee">
+    </a>
+  </div>
+</template>
