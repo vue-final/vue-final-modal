9485c6a5139a5ec7f4ae5939980303c22a100d55
Hunter <hunterliu1003@gmail.com>
2023-03-09 21:22:48 +0800

    docs: remove CodeBlockFile

--- docs/markdown-parser/handler/blockquote.ts
+++
@@ -1,7 +0,0 @@
-import type { H } from 'mdast-util-to-hast'
-import { all } from 'mdast-util-to-hast'
-import { wrap } from './utils'
-
-export default function blockquote(h: H, node: any) {
-  return h(node, 'blockquote', wrap(all(h, node), true))
-}
