d70b2eda1c789af99d17d15d8da16eaaf0de8eb6
Negezor <negezor@gmail.com>
2023-07-02 02:21:06 +1100

    feat(modal): implement reserveScrollBarGap props

--- packages/vue-final-modal/src/components/CoreModal/CoreModalProps.ts
+++ packages/vue-final-modal/src/components/CoreModal/CoreModalProps.ts
@@ -152,6 +152,13 @@ export const coreModalProps = {
   lockScroll: {
     type: Boolean as PropType<boolean>,
     default: true,
+  /**
+   * @description Creates a padding-right when scroll is locked to prevent the page from jumping
+   * @default `true`
+   */
+  reserveScrollBarGap: {
+    type: Boolean as PropType<boolean>,
+    default: true,
   },
   /**
    * @description Define how to increase the zIndex when there are nested modals
