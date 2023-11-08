cafb3677a02e8da6bb40d9ee7da55a8c83d8ddb2
Daniel Roe <daniel@roe.dev>
2023-11-08 11:05:25 +0000

    fix: import nuxt composables from #imports (#398)

--- packages/nuxt/src/runtime/plugin.ts
+++ packages/nuxt/src/runtime/plugin.ts
@@ -1,5 +1,5 @@
 import { createVfm } from 'vue-final-modal'
-import { defineNuxtPlugin } from '#app'
+import { defineNuxtPlugin } from '#imports'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vfm = createVfm()
