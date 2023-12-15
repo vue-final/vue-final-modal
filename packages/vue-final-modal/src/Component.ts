fec4f4786f568d313d35728c2dea1960a0ee6338
Hunter <hunterliu1003@gmail.com>
2023-12-15 12:27:41 +0800

    fix: fixed type ComponentEmit

--- packages/vue-final-modal/src/Component.ts
+++ packages/vue-final-modal/src/Component.ts
@@ -18,10 +18,10 @@ T extends new () => { $slots: infer S } ? NonNullable<S> :
   T extends (props: any, ctx: { slots: infer S; attrs: any; emit: any }, ...args: any) => any ? NonNullable<S> :
       {}
 
-// export type ComponentEmit<T> =
-// T extends new () => { $emit: infer E } ? NonNullable<E> :
-//   T extends (props: any, ctx: { slots: any; attrs: any; emit: infer E }, ...args: any) => any ? NonNullable<E> :
-//       {}
+export type ComponentEmit<T> =
+T extends new () => { $emit: infer E } ? NonNullable<E> :
+  T extends (props: any, ctx: { slots: any; attrs: any; emit: infer E }, ...args: any) => any ? NonNullable<E> :
+      {}
 
 // export type ComponentExposed<T> =
 // T extends new () => infer E ? E :
